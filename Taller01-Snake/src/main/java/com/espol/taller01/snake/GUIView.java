9537092c07648dde064184caee42c7b1b9adf387
fzavala2003 <127550614+fzavala2003@users.noreply.github.com>
2024-05-22 06:32:15 -0500

    f

--- Taller01-Snake/src/main/java/com/espol/taller01/snake/GUIView.java
+++ Taller01-Snake/src/main/java/com/espol/taller01/snake/GUIView.java
@@ -50,7 +50,7 @@ public class GUIView extends JPanel {
 		this.gameFactory = factory;
 
 		// Set the background on the GameView
-		this.gameView.setBackground(Color.BLUE);
+		this.gameView.setBackground(Color.yellow);
 
 		// Set the layout on myself
 		setLayout(new BorderLayout());
@@ -63,7 +63,7 @@ public class GUIView extends JPanel {
 
 		// Create a new button on that panel and add a StartGameListener as
 		// listener on that button
-		this.startGameButton = new JButton("Playgame");
+		this.startGameButton = new JButton("Playgroung");
 		this.startGameButton.addActionListener(new StartGameListener());
 		this.guiPanel.add(this.startGameButton);
 
